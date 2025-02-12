51a41492c3de5aa1ee593d397313a2b56c24744f
igrom <igro.men.05@bk.ru>
2025-02-12 11:25:05 +0500

    Первое задание из блока C#

--- PracticTaxtelecom/Program.cs
+++ PracticTaxtelecom/Program.cs
@@ -10,7 +10,29 @@ namespace PracticTaxtelecom
     {
         static void Main(string[] args)
         {
+            string[] array = { "a", "abcdef", "abcde" };
 
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length % 2 == 0)
+                {
+                    string first = array[i].Substring(0, array[i].Length / 2);
+                    string second = array[i].Substring(array[i].Length / 2);
+
+                    Console.WriteLine($"{ReverseString(first) + ReverseString(second)}");
+                }
+                else
+                {
+                    Console.WriteLine($"{ReverseString(array[i]) + array[i]}");
+                }
+            }
+            Console.ReadKey();
+        }
+        static string ReverseString(string s)
+        {
+            char[] chars = s.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
         }
     }
 }
